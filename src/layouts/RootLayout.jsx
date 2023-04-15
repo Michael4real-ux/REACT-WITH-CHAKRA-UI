9d8bbafd0dda1f4475b6d5d50745a0f4f71ab187
Damilola Babalola <dammydeji.dd@gmail.com>
2023-04-15 22:08:14 +0100

    Responsive styles

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,16 +7,16 @@ export default function RootLayout() {
     <Grid templateColumns="repeat(6,1fr)" bg="gray.50">
       <GridItem
       as="aside"
-      colSpan="1"
+      colSpan={{ base:6, lg:2, xl:1 }}
       bg="purple.400"
-      minHeight="100vh"
-      p="30px"
+      minHeight={{ lg:'100vh' }}
+      p={{ base:'20px', lg:'30px' }}
       >
         <span>Sidebar</span>
       </GridItem>
       <GridItem
       as="main"
-      colSpan="5"
+      colSpan={{ base:6, lg:4, xl:5 }}
       p="40px"
       >
         <Navbar />
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid } from '@chakra-ui/react'
+import { Box, SimpleGrid, Text } from '@chakra-ui/react'
 
 export default function Dashboard() {
   // const boxStyles = {
@@ -17,7 +17,9 @@ export default function Dashboard() {
 
   return (
      <SimpleGrid p={10} spacing={10} minChildWidth={250}>
-       <Box bg="white" h="200px" border="1px solid"></Box>
+       <Box bg="white" h="200px" border="1px solid">
+        <Text color={{ base:'pink', md:'blue', lg:'green' }}>Hello</Text>
+       </Box>
        <Box bg="white" h="200px" border="1px solid"></Box>
        <Box bg="white" h="200px" border="1px solid"></Box>
        <Box bg="white" h="200px" border="1px solid"></Box>
